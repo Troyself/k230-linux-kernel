04819a012a283ae6d880ef842a5e8aa07081f1f7
Xukai Wang <kingxukai@zohomail.com>
2024-11-30 19:17:53 +0800

    modify BUG() to WARN_ON_ONCE()
    Signed-off-by: Xukai Wang <kingxukai@zohomail.com>

--- drivers/clk/clk-k230.c
+++ drivers/clk/clk-k230.c
@@ -4,7 +4,6 @@
  * Author: Troy Mitchell <troymitchell988@gmail.com>
  * Kendryte Canaan K230 Clock Drivers
  */
-#include <linux/bug.h>
 #include <linux/bitfield.h>
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
@@ -872,7 +871,7 @@ static int k230_clk_find_approximate(struct k230_clk *clk,
 		break;
 
 	default:
-		BUG();
+		WARN_ON_ONCE(true);
 		return -EPERM;
 	}
 	return 0;
@@ -1149,7 +1148,7 @@ static int _k230_clk_mux_get_hw(struct k230_sysclk *ksc,
 		*hw = &ksc->clks[pclk->clk_id].hw;
 		break;
 	default:
-		BUG();
+		WARN_ON_ONCE(true);
 		return -EPERM;
 	}
 	return 0;
